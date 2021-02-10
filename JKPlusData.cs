d21c8c36e091781e22cb835b150673814133079a
Phoenixx19 <41084138+Phoenixx19@users.noreply.github.com>
2021-02-10 18:22:24 +0100

    v1.3.1 Update

--- JKPlusData.cs
+++ JKPlusData.cs
@@ -7,7 +7,7 @@ namespace JumpKingPlus
 
         public static class JKVersion
         {
-            public static Version version = new Version("1.3.0");
+            public static Version version = new Version("1.3.1");
         }
         /// <summary>
         ///     recap on JKPlusData.
