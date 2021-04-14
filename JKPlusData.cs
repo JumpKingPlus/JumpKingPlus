1027392464a14e52409d9fb6a782c5678946abd1
Phoenixx19 <41084138+Phoenixx19@users.noreply.github.com>
2021-04-14 22:45:45 +0200

    v1.4.2 Release

--- JKPlusData.cs
+++ JKPlusData.cs
@@ -7,7 +7,7 @@ namespace JumpKingPlus
 
         public static class JKVersion
         {
-            public static Version version = new Version("1.4.1");
+            public static Version version = new Version("1.4.2");
         }
         /// <summary>
         ///     recap on JKPlusData.
